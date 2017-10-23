b9bbb0f1240a629c9e0daad76e21adcd866808b4
alx9r <eggy.island9@gmail.com>
2017-10-22 17:15:56 -0700

    add disabling of Schannel Ciphers, Hashes, and KeyExchangeAlgorithms to prerequisites

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -2,4 +2,17 @@
 
 Import-WebModule 'https://github.com/pester/Pester/archive/4.0.8.zip' 'Pester.psd1' -PassThru
 
+Write-Host '=== Disabling Schannel Ciphers, Hashes, and KeyExchangeAlgorithms ==='
+[BootstraPS.Schannel.Ciphers].GetEnumValues() |
+    ? { $_ -match 'RC4' } |
+    % {
+        Write-Host "Disabling Cipher $_"
+        Clear-SchannelRegistryEntry Enabled -Cipher $_
+    }
+Write-Host 'Disabling Hash MD5'
+Clear-SchannelRegistryEntry Enabled -Hash MD5
+Write-Host 'Disabling KeyExchangeAlgorithm'
+Clear-SchannelRegistryEntry Enabled -KeyExchangeAlgorithm DH
+
+
 Remove-Module BootstraPS
