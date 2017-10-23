ca8a7ed12c40d621e8c083b1774776801a78bc64
alx9r <eggy.island9@gmail.com>
2017-10-22 18:44:03 -0700

    update static files

--- webLoad.ps1
+++ webLoad.ps1
@@ -1,13 +1,13 @@
 "$([System.IO.Path]::GetTempPath())\BootstraPS.psm1" |
     % {
-        Invoke-WebRequest https://raw.githubusercontent.com/alx9r/BootstraPS/12ae959670e648c40736ca7f4afd9508d18e0abb/BootstraPS.psm1 -OutFile $_ |
+        Invoke-WebRequest https://raw.githubusercontent.com/alx9r/BootstraPS/4e2a6a8d0e1b615f9d2f4689f3dcc3f605f4d388/BootstraPS.psm1 -OutFile $_ |
             Out-Null
         $_
         Remove-Item $_
     } |
     % {
         Get-FileHash $_ -Algorithm SHA512 |
-            ? {$_.Hash -ne 'A43AA05320DC17D0A241BD9876EB145593486CF46B5EA71C059B045DFF193E28753041384EA2D000B9BF146C356BB12A8AA2CB8D3EF688A8EE528275D192EC3B' } | 
+            ? {$_.Hash -ne 'AB2DD2E54C7E89186F9FAC612B0CFF4D81F16BB65BEEEA648C0F08925B3D91AD12513A8D40C417F0D4C6F596BEC6FD781A338C75BC2769E0F6F9734FC6B8533A' } | 
             % { throw 'Failed hash check.' }
         $_ | Import-Module
     }
