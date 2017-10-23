c8438636f22d1e1f3aa6eb77463b2b1637d3fcc4
alx9r <eggy.island9@gmail.com>
2017-10-23 15:55:01 -0700

    Set-SchannelPolicy -Strict prior to calling Import-Webmodule

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -1,8 +1,4 @@
 . "$PSScriptRoot\webLoad.ps1"
-
-Import-WebModule 'https://github.com/pester/Pester/archive/4.0.8.zip' 'Pester.psd1' -PassThru
-
-Write-Host '=== Set-SchannelPolicy -Strict ==='
 Set-SchannelPolicy -Strict
-
+Import-WebModule 'https://github.com/pester/Pester/archive/4.0.8.zip' 'Pester.psd1' -PassThru
 Remove-Module BootstraPS
