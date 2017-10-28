d9136eb7e673abf8f18becd3ac6a8f59a683226e
alx9r <eggy.island9@gmail.com>
2017-10-28 13:53:16 -0700

    improve -CertificateValidator help

--- BootstraPS.psm1
+++ BootstraPS.psm1
@@ -942,7 +942,7 @@ function Save-WebFile
 	The path to save the file.
     
 	.PARAMETER CertificateValidator
-	A scriptblock that is invoked by the system when connecting to Uri.  CertificateValidator's output tells the system whether the certificate is valid.  The system interprets the certificate to be valid if all outputs from CertificateValidator are $true.  If any output is $false or a non-boolean value, the system interprets the certificate to be invalid which causes Save-WebFile to throw an exception without downloading any file.
+	A scriptblock that is invoked by the system when connecting to Uri.  CertificateValidator's output tells the system whether the certificate is valid.  The system interprets the certificate to be valid if all outputs from CertificateValidator are $true.  If any output is $false or a non-boolean value, the system interprets the certificate to be invalid which causes Save-WebFile to throw an exception without downloading any file.  The automatic variable $_ is available in the scriptblock and has the properties sender, certificate, chain, and sslPolicyErrors whose values are set from the arguments of the System.Net.Security.RemoteCertificateValidationCallback delegate.
 
     .PARAMETER SkipSecurityPolicyCheck
     Setting SkipSecurityPolicyCheck can reduce the security of the connection Save-WebFile makes with a server. When SkipSecurityPolicyCheck is set, Save-WebFile skips checks asserting that the networking subsystems used to make https connections has certain cryptographic policies applied.
