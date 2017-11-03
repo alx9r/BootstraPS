02a1ec32ad1b4713332558e7509351b343f6cc60
alx9r <eggy.island9@gmail.com>
2017-11-02 17:06:21 -0700

    organize

--- IntegrationTests/certificatePermissiveness.Tests.ps1
+++ IntegrationTests/certificatePermissiveness.Tests.ps1
@@ -7,10 +7,23 @@ Describe 'certificate permissiveness' {
     Set-SpManagerPolicy -Strict
     Context 'fail without additional validation' {
         It '<u>' -TestCases @(
-            @{u='https://expired.badssl.com'}
-            @{u='https://wrong.host.badssl.com'}
-            @{u='https://self-signed.badssl.com'}
-            @{u='https://untrusted-root.badssl.com/'}
+            @(
+                # Not Secure
+                'expired'
+                'wrong.host'
+                'self-signed'
+                'untrusted-root'
+                'no-san'
+                'null'
+                ## known-bad
+                'dsdtestprovider'
+                'edellroot'
+                'mitm-software'
+                'preact-cli'
+                'superfish'
+                'webpack-dev-server'
+            ) |
+                % {@{u="https://$_.badssl.com"}}
         ) {
             param($u,$cv)
             try
@@ -21,17 +34,41 @@ Describe 'certificate permissiveness' {
             {
                 $threw = $true
                 $_.Exception | CoalesceExceptionMessage |
-                    Should -Match 'certificate is invalid'
+                    Should -Match '(certificate is invalid|Could not create)'
             }
             $threw | Should -Be $true
         }
     }
     Context 'succeed without additional validation' {
         It '<u>' -TestCases @(
-            @{u='https://sha1-intermediate.badssl.com/'}
-            @{u='https://revoked.badssl.com'}
-            @{u='https://pinning-test.badssl.com'}
-            @{u='https://invalid-expected-sct.badssl.com/'}
+            @(
+                # Not Secure
+                'sha1-intermediate' # <-- requires intervention
+                'revoked'           # <-- requires intervention
+
+                # Bad Certificates
+                'pinning-test'      # If we were a browser or any other thing that had a long
+                                    # lifecycle, this should fail.
+                'invalid-expected-sct' # <-- requires intervention
+
+                # Secure but Weird
+                '1000-sans'
+                 #'10000-sans'
+                'rsa8192'
+                'no-subject'
+                'no-common-name'
+                'incomplete-chain'
+
+                # Secure
+                'sha256'
+                'sha384'
+                'sha512'
+                'rsa2048'
+                'ecc256'
+                'ecc384'
+                 #'mozilla-modern'
+            ) |
+                % {@{u="https://$_.badssl.com"}}
         ) {
             param($u,$cv)
             $u | Save-WebFile ([System.IO.Path]::GetTempFileName())
