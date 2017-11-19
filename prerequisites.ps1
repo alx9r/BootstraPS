2e27e19e82dff7e7c0ae75a0dac0e244a836c9d2
alx9r <eggy.island9@gmail.com>
2017-11-19 13:55:04 -0800

    use Assert-Throw and Get-ChildException

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -2,4 +2,5 @@
 Set-SchannelPolicy -Strict
 Set-SpManagerPolicy -Strict
 Import-WebModule 'https://github.com/pester/Pester/archive/4.0.8.zip' 'Pester.psd1' -PassThru
+Import-WebModule 'https://github.com/alx9r/Assert/archive/7847b2dadc50f8ba0fe6b3b4c9e4dad0de47b17d.zip' -PassThru
 Remove-Module BootstraPS
